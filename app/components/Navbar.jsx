06278ed391b074d2174b1bc7dd84e07aa04898f4
Dinesh Priyantha <Priyanthadinesh2002@gmail.com>
2025-03-10 11:27:52 +0530

    Up:navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <div>
         <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]'>
-            <Image src={assets.header_bg_color} className='w-full'/>
+            <Image src={assets.header_bg_color} className='w-full' alt=''/>
             </div>
 
         <nav className='w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between  z-50'>
@@ -31,12 +31,51 @@ const Navbar = () => {
                 </li>
                 
             </ul>
-            <div>
+            <div className='flex items-center gap-4'>
+
+                <button >
+                    <Image src={assets.moon_icon} alt='' className='w-6'/>
+                </button>
             
                     <a href="#contact" className="hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full
                      ml-4">Contact<Image src={assets.arrow_icon} className='w-3' alt='contact'/></a>
+
+                
+                <button className='block md:hidden ml-3' >
+                    <Image src={assets.menu_black} alt='' className='w-6'/>
+                </button> 
                 
                 </div>
+
+       {/* Mobile menu */}         
+
+       <ul className='flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-0 top-0 bottom-0 w-64 z-50 h-screen bg-bl   transition duration-500 '>
+      
+      <div className='absolute right-6 top-6'>
+        <Image src={assets.close_black} alt='' className=' w-5 cursor-pointer'/>
+      </div>
+
+
+       <li className="font-Ovo">
+                    <a href="#top" className="text-black">Home</a>
+                </li>
+                <li className="">
+                    <a href="about" className="text-black">About me</a>
+                </li>
+                <li className="">
+                    <a href="services" className="text-black">Services</a>
+                </li>
+                <li className="">
+                    <a href="#work" className="text-black">Portfolio</a>
+                </li>
+                <li className="">
+                    <a href="#contact" className="text-black">Contact me</a>   
+                </li>
+       </ul>
+   
+
+
+
         </nav>
        
     </div>
